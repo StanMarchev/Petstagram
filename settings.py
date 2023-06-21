3bf98df7921331cf3784cb36ba8152824d578328
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-06-21 14:29:58 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "petstagram",
         "USER": "postgres",
-        "PASSWORD": "bgiknow123",
+        "PASSWORD": "*******",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
